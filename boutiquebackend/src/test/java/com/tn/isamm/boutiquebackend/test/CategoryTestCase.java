a0b4c87f624943444585c348f327aad5c06db70d
NahedDakhli <dakhlinahed5@gmail.com>
2017-12-27 17:09:26 +0100

    test junit

--- boutiquebackend/src/test/java/com/tn/isamm/boutiquebackend/test/CategoryTestCase.java
+++ boutiquebackend/src/test/java/com/tn/isamm/boutiquebackend/test/CategoryTestCase.java
@@ -30,7 +30,7 @@ public class CategoryTestCase {
 		category = new Category();
 		
 		category.setName("Souris");
-		category.setDescription("souriiiiiiiiiiiiiis");
+		category.setDescription("souriiiiiiiiiiiiiiis");
 		category.setImageURL("nahed10.jpg");
 		
 		assertEquals("Successfully added a category inside the table!",true,categoryDAO.add(category));
